85826a071b74721935028f36757ebd77b54f9322
William Hamon <bearwrh@gmail.com>
2023-10-25 16:12:32 -0400

    add decode function and fix some things

--- cowLab.py
+++ cowLab.py
@@ -15,20 +15,25 @@ def encode(password_encode):
     encoded2 = ''.join(map(str, encoded))
     return encoded2
 
+def decode(password):
+    return ''.join(str((int(i) - 3) % 10) for i in password)
 
-if __name__ == "__main__":
-    running = True
-    while running:
-        print(f"Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+def main():
+    encoded = None
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
         answer = int(input("Please enter an option: "))
         if answer == 1:
-            password_encode = input("Please enter your password to encode: ")
+            encoded = encode(input("Please enter your password to encode: "))
             print("Your password has been encoded and stored!\n")
         elif answer == 2:
-            coded = encode(password_encode)
-            print(f"The encoded password is {coded}, and the original password is {password_encode}.\n")
+            if encoded == None:
+                print("No password has been encoded!\n")
+            else:
+                decoded = decode(encoded)
+                print(f"The encoded password is {encoded}, and the original password is {decoded}.\n")
         elif answer == 3:
-            running = False
+            break
 
-
-
+if __name__ == "__main__":
+    main()
